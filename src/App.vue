9046241fc73eedf386c83c7c188e40d4da0e1246
AlexandreHiroyuki <alexandrehiroyuki@hotmail.com>
2019-05-21 18:35:42 -0300

    Vue CLI Update and Icon Update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app" class="bg-light-yellow helvetica">
     <header class="bg-light-blue shadow-1 flex flex-wrap">
-      <img alt="ENC Logo" src="./assets/logo.png" class="dib w-10 h3">
-      <h1 class="dib f2 mb2 b w-90">Energy Network Control - Internet of Things</h1>
+      <h1 class="sans-serif dib f2 mb2 b w-90 pl3">Energy Network Control - Internet of Things</h1>
 
       <div class="bg-lightest-blue w-100">
         <router-link
